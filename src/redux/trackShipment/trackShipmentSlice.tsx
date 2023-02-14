20d384a42e93d21bcba508c5ffe9ab8d03b76765
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2023-02-14 15:00:30 +0200

    add redux and fetch data from Api

---
+++ src/redux/trackShipment/trackShipmentSlice.tsx
@@ -0,0 +1,49 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+
+export const getShipmentStatus = createAsyncThunk(
+  'shipments/track/trackingNumber',
+  // if you type your function argument here
+  async (trackingNumber: number) => {
+    const response = await fetch(`https://tracking.bosta.co/shipments/track/${trackingNumber}`,{
+      method:"GET"
+    })
+    const result = response.json()
+    return result
+  }
+)
+
+export interface ShipmentState {
+  data: []
+  loading: boolean
+  error: string | null;
+}
+
+const initialState: ShipmentState = {
+  data: [],
+  loading: false,
+  error: null,
+}
+
+export const trackShipmentSlice = createSlice({
+  name: 'trackShipment',
+  initialState,
+  reducers: {
+  },
+  extraReducers:(builder) => {
+    builder.addCase(getShipmentStatus.pending,(state,action)=>{
+      state.loading = true;
+    })  
+    builder.addCase(getShipmentStatus.fulfilled,(state,action)=>{
+      state.loading = false;
+      state.data = action.payload
+    })  
+    builder.addCase(getShipmentStatus.rejected,(state, action: PayloadAction<any>)=>{
+      state.error = action.payload;
+    })  
+  },
+})
+
+// Action creators are generated for each case reducer function
+//export const {  } = trackShipmentSlice.actions
+
+export default trackShipmentSlice.reducer
