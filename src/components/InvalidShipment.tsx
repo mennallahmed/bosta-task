3f3744032f85ec516ffff75a4b4193cd3b04b76a
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2023-02-15 14:37:17 +0200

    edit invalid shipment width

--- src/components/InvalidShipment.tsx
+++ src/components/InvalidShipment.tsx
@@ -2,7 +2,7 @@ import warningIcon from "../assets/warning-icon.svg";
 
 const InvalidShipment = () => {
   return (
-    <div className="container flex items-start gap-3 mx-auto w-11/12 md:w-3/5 bg-[#fef3f2] text-xl font-semibold border border-[#fecdca] mt-24 p-4 md:p-10 rounded-2xl">
+    <div className="container flex items-start gap-3 mx-auto w-11/12 lg:w-3/4 bg-[#fef3f2] text-xl font-semibold border border-[#fecdca] mt-24 p-4 md:p-10 rounded-2xl">
       <img src={warningIcon} alt="warningIcon" />
       <p>
         No record of this tracking number can be found at this time, please
